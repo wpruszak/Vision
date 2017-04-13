2f307c1a031ea0c8c9f8ea6b289ee60d0a9943a4
Wojciech Pruszak <info@wpruszak.com>
2017-04-13 19:19:44 +0200

    Initial commit.

---
+++ src/main/java/com/wpruszak/vision/Application.java
@@ -0,0 +1,10 @@
+package com.wpruszak.vision;
+
+/**
+ * {@author Wojciech Pruszak} <info@wpruszak.com> on 13.04.17.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+    }
+}
